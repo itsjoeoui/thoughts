894a51167e38386bf895ec068274431199b97b44
Joey Yu <joey@itsjoeoui.com>
2023-02-17 00:41:17 -0500

    Basic layouts for mobile

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        blog: {
+          100: "#cdb4db",
+          200: "#ffc8dd",
+          300: "#ffafcc",
+          400: "#bde0fe",
+          500: "#a2d2ff",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
